57397102e41a455804fab60913993d77b6241bb3
JordyPirata <jordi77alva@gmail.com>
2024-02-23 11:06:58 -0600

    Add JSONserializer

--- Assets/Scripts/Serializer/Serializer.cs
+++ Assets/Scripts/Serializer/Serializer.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
-public class Serializer 
+public class Serializer
 {
+    public static void Serialize<T>(T data, string path)
+    {
+        string json = JsonConvert.SerializeObject(data);
+        File.WriteAllText(path, json);
+    }
 
+    public static T Deserialize<T>(string path)
+    {
+        string json = File.ReadAllText(path);
+        return JsonConvert.DeserializeObject<T>(json);
+    }
 }
