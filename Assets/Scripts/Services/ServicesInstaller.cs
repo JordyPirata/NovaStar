b40c8f8ae73385a95f92e9ce68913ae0d20846e5
JordyPirata <jordi77alva@gmail.com>
2024-06-13 22:04:56 -0600

    Fix MonoBehavior init

--- Assets/Scripts/Services/ServicesInstaller.cs
+++ Assets/Scripts/Services/ServicesInstaller.cs
@@ -7,13 +7,12 @@ public class ServiceInstaller : MonoBehaviour
     private void Awake()
     {
         InstallServices();
-        gameObject.AddComponent<PlayerInfo>();
     }
     public void InstallServices()
     {
         ServiceLocator.Register<ISettingsService>(new SettingsService());
         ServiceLocator.Register<IWeldMap>(new WeldMap());
-        ServiceLocator.Register<IPlayerInfo>(new PlayerInfo());
+        ServiceLocator.Register<IPlayerInfo>(gameObject.AddComponent<PlayerInfo>());
         ServiceLocator.Register<IMapGenerator>(new MapGenerator());
         ServiceLocator.Register<IRepository>(new GameRepository());
     }
