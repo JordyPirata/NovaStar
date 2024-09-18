72c1106e7f70b7b100e6a79473b941051b4ec36d
JordyPirata <jordi77alva@gmail.com>
2024-09-18 08:12:49 -0600

    Refactor HUDHolder to handle scene loading and initialize UI elements

--- Assets/Scripts/Services/HUDHolder.cs
+++ Assets/Scripts/Services/HUDHolder.cs
@@ -3,20 +3,34 @@ using UnityEngine;
 using UI.Hud;
 using Services.Interfaces;
 using System;
+using UnityEngine.SceneManagement;
 
 namespace Services
 {
     [Serializable]
     public class HUDHolder : MonoBehaviour, IHUDService
     {
-        void Start()
+        // TODO: Make game Event System to handle this
+        void Awake()
         {
-            healthBar = GetComponentInChildren<HealthBar>();
-            staminaBar = GetComponentInChildren<StaminaBar>();
-            hungerBar = GetComponentInChildren<HungerBar>();
-            thirstBar = GetComponentInChildren<ThirstBar>();
-            miniMap = GetComponentInChildren<MiniMap>();
-            freezeScreen = GetComponentInChildren<FreezeScreen>();
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                switch (scene.name)
+                {
+                    case "Game":
+                        healthBar = GetComponentInChildren<HealthBar>();
+                        if (healthBar == null)
+                        {
+                            Debug.LogError("HealthBar not found");
+                        }
+                        staminaBar = GetComponentInChildren<StaminaBar>();
+                        hungerBar = GetComponentInChildren<HungerBar>();
+                        thirstBar = GetComponentInChildren<ThirstBar>();
+                        miniMap = GetComponentInChildren<MiniMap>();
+                        freezeScreen = GetComponentInChildren<FreezeScreen>();
+                        break;
+                }
+            };
         }
         // Restrict the value to be between 0 and 1
         public float HealthValue
